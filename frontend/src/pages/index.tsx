66bf161fe65d748e3e9457a74f94e7ea01403004
kuboryu <kbtrg.527@gmail.com>
2023-11-23 17:11:25 +0900

    feat: 🎸 [index.tsx] typescript化

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,37 +1,64 @@
-import Error from 'next/error';
+import useSWR from "swr"
+import axios, { AxiosResponse } from "axios";
+import { Fragment } from "react";
 
-const Home = ({ posts, statusCode }) => {
-  if (statusCode) {
-    return <Error statusCode={statusCode} />;
-  }
+type ApiData = {
+  posts: Post[]
+}
+
+type Post = {
+  id: number
+  title: string
+  body: string
+  created_at: string
+  updated_at: string
+}
+
+const Home = () => {
+  const { data: datas, error, isLoading } = useSWR(
+    `${process.env.NEXT_PUBLIC_CORE_URL}/posts`,
+    fetcher<ApiData>
+  )
+
+  if (error) return <p>Failed to load.</p>
+  if (isLoading) return <p>Loading...</p>
+  if (!datas) return null
 
   return (
-    <div>
-      <h1>POSTS</h1>
-      <ul>
-        {!posts
-          ? null
-          : posts.map((post, index) => {
-            return <li key={index}>{post.title}</li>;
-          })}
-      </ul>
-    </div>
-  );
-};
+    <>
+      {datas.posts.map((data, i) => (
+        <Fragment key={i}>
+          <p>id: {data.id}</p>
+          <p>id: {data.title}</p>
+          <p>id: {data.body}</p>
+        </Fragment>
+      ))}
+    </>
+  )
+}
 
-Home.getInitialProps = async (context) => {
+const fetcher = async <T,>(url: string): Promise<T> => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_CORE_URL}/posts`);
-    const data = await res.json();
-    
-    return {
-      posts: data.posts,
-    };
-  } catch (error) {
-    return {
-      statusCode: error.response ? error.response.status : 500,
-    };
+    const res: AxiosResponse<T> = await axios.get(url)
+    return res.data
+  } catch (e) {
+    // axiosの型ガード
+    if (axios.isAxiosError(e)) {
+      if (e.response && e.response.status === 400) {
+        // サーバーからのステータスコードがエラーの場合
+        console.log("Response error:", e.response.data);
+        console.log("Status code:", e.response.status);
+      } else if (axios.isAxiosError(e) && e.request) {
+        // リクエストが行われたが、サーバーからの応答がない場合
+        console.log("No response received");
+      } else {
+        // リクエストがすら行われなかった場合
+        console.log("Request error:", e.message);
+      }
+    }
+
+    throw e
   }
-};
+}
 
 export default Home;
